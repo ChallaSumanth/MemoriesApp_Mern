e806033e7ba8255030b9c04826db455aff40cfe3
Sumanth <challasumanth44@gmail.com>
2023-01-30 23:00:31 +0530

    first commit

---
+++ client/build/precache-manifest.f12467ec97d6e58d94bdef2c2ba5e4cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171bbbe994aa58aa05580dff3d04710a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b5a0f5da8df2f8fd81",
+    "url": "/static/css/main.958a92ee.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b5a0f5da8df2f8fd81",
+    "url": "/static/js/main.7ebec878.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
