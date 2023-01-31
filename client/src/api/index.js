bce1fa8957cec50aeb3210d1e94c5851629ed1df
Sumanth <challasumanth44@gmail.com>
2023-01-31 10:20:37 +0530

    path update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://upset-glasses-hare.cyclic.app/';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
